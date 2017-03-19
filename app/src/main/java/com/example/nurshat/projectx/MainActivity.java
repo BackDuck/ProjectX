691176d9188362395e32d6cce2fe7e4479bc9421
Nurshat Nizamov <q7ace@ya.ru>
2017-03-19 17:57:48 +0300

    lol

--- app/src/main/java/com/example/nurshat/projectx/MainActivity.java
+++ app/src/main/java/com/example/nurshat/projectx/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("hello world");
 
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
